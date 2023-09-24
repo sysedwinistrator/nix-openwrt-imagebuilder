7e0159c284bd29a4205fb976d1a8a093c402eb0c
Edwin Mackenzie-Owen <edwin.mowen@gmail.com>
2023-09-24 14:29:53 +0200

    Revert "generate-all-hashes: start ignoring -rc releases"
    This reverts commit 67ce3d103cb29c33e781d9fa219651799e7db09e.

--- generate-all-hashes.nix
+++ generate-all-hashes.nix
@@ -8,9 +8,8 @@ writeScriptBin "generate-all-hashes" ''
   PATH=${lib.makeBinPath [ self.packages.${system}.generate-hashes git ]}:$PATH
 
   RELEASES=$(curl -s https://downloads.openwrt.org |
-    grep -oP "OpenWrt [1-9][0-9\.]+<" |
-    sed -e 's/OpenWrt //' -e 's/<//' |
-    uniq
+    grep -oP "OpenWrt [1-9][0-9\.\-a-zA-Z]+" |
+    sed -e 's/OpenWrt //'
   )
   for RELEASE in $RELEASES snapshot ; do
     echo "# Fetching hashes for OpenWrt $RELEASE"
